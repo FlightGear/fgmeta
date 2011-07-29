ecc81119868ae90f9ddae72eedc1c03bfbcda2ea
Torsten Dreyer <Torsten@t3r.de>
2011-07-29 14:12:32 +0200

    Fix typo in hudson win32/64 build scripts
    Also remove obsolete build of SimGear which is included
    in the build of the solution.

--- hudson_win_build32.bat
+++ hudson_win_build32.bat
@@ -1,11 +1,7 @@
-
 IF NOT DEFINED WORKSPACE SET WORKSPACE=%~dp0
 
-SET /P SIMGEAR_VERSION=<%WORKSAPCE%\simgear\version
+SET /P SIMGEAR_VERSION=<%WORKSPACE%\simgear\version
 ECHO #define SIMGEAR_VERSION "%SIMGEAR_VERSION%" > %WORKSPACE%\simgear\simgear\version.h
-
-cd %WORKSPACE%\simgear\projects\VC90
-msbuild SimGear.vcproj /p:Configuration=Release /m
 
 cd %WORKSPACE%\flightgear
 call scripts\tools\version.bat
