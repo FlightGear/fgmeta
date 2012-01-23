a3f33c5e9750867d070aee6c1e20802205a24e75
Frederic Bouvier <fredfgfs01@free.fr>
2012-01-23 09:04:47 +0100

    First try at building Windows release with VS2010 and Cmake

--- hudson_win_build32.bat
+++ hudson_win_build32.bat
@@ -1,12 +1,16 @@
 
 IF NOT DEFINED WORKSPACE SET WORKSPACE=%~dp0
 
-ECHO #define SIMGEAR_VERSION "2.2.0" > %WORKSPACE%\simgear\simgear\version.h
-cd %WORKSPACE%\simgear\projects\VC90
-msbuild SimGear.vcproj /p:Configuration=Release /m
+ECHO #define SIMGEAR_VERSION "2.6.0" > %WORKSPACE%\simgear\simgear\version.h
+rem xcopy D:\FGFSHudson\boost_1_44_0\* D:\FGFSHudsonCMake\SimGear\boost_1_44_0\ /s /y /q
+set PATH=%PATH%;D:\Program Files (x86)\CMake 2.8\bin
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\Tools\vsvars32.bat"
+md build-sg
+md build-fg
+cd build-sg
+cmake ..\simgear -G "Visual Studio 10" -DMSVC_3RDPARTY_ROOT=%WORKSPACE% -DCMAKE_INSTALL_PREFIX:PATH=%WORKSPACE%/install/msvc100/SimGear
+cmake --build . --config Release --target INSTALL
 
-cd %WORKSPACE%\flightgear
-call scripts\tools\version.bat
-SET HAVE_VERSION_H=1
-cd %WORKSPACE%\flightgear\projects\VC90
-msbuild FlightGear.sln /p:Configuration=Release /m
+cd ..\build-fg
+cmake ..\flightgear -G "Visual Studio 10" -DMSVC_3RDPARTY_ROOT=%WORKSPACE% -DCMAKE_INSTALL_PREFIX:PATH=%WORKSPACE%/install/msvc100/FlightGear -DFLTK_FLUID_EXECUTABLE=%WORKSPACE%/3rdParty/bin/fluid.exe
+cmake --build . --config Release --target INSTALL
