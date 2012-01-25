b61f1403dbd36c56adb23a15d350ab08a209e8e8
Frederic Bouvier <fredfgfs01@free.fr>
2012-01-25 21:58:41 +0100

    Second try at building a 64-bit version of the release : flightgear

--- hudson_win_build64.bat
+++ hudson_win_build64.bat
@@ -5,11 +5,11 @@ ECHO #define SIMGEAR_VERSION "2.6.0" > %WORKSPACE%\simgear\simgear\version.h
 rem set PATH=%PATH%;D:\Program Files (x86)\CMake 2.8\bin
 rem call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" amd64
 md build-sg64
-rem md build-fg64
+md build-fg64
 cd build-sg64
 cmake ..\SimGear -G "Visual Studio 10 Win64" -DMSVC_3RDPARTY_ROOT=%WORKSPACE% -DCMAKE_INSTALL_PREFIX:PATH=%WORKSPACE%/install/msvc100-64/SimGear
 cmake --build . --config Release --target INSTALL
 
-rem cd ..\build-fg64
-rem cmake ..\flightgear -G "Visual Studio 10" -DMSVC_3RDPARTY_ROOT=%WORKSPACE% -DCMAKE_INSTALL_PREFIX:PATH=%WORKSPACE%/install/msvc100/FlightGear -DFLTK_FLUID_EXECUTABLE=%WORKSPACE%/3rdParty/bin/fluid.exe
-rem cmake --build . --config Release --target INSTALL
+cd ..\build-fg64
+cmake ..\flightgear -G "Visual Studio 10 Win64" -DMSVC_3RDPARTY_ROOT=%WORKSPACE% -DCMAKE_INSTALL_PREFIX:PATH=%WORKSPACE%/install/msvc100-64/FlightGear -DFLTK_FLUID_EXECUTABLE=%WORKSPACE%/3rdParty/bin/fluid.exe
+cmake --build . --config Release --target INSTALL
