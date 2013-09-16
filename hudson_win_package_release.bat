51b9e6166788ac896acf10d58af63a7f54e86984
James Turner <zakalawe@mac.com>
2013-09-16 15:15:13 +0100

    Make OpenThreads SO number dynamic.

--- hudson_win_package_release.bat
+++ hudson_win_package_release.bat
@@ -17,14 +17,17 @@ set PATH=%PATH%;C:\Program Files\7-zip
 REM indirect way to get command output into an environment variable
 osgversion --so-number > %TEMP%\osg-so-number.txt
 osgversion --version-number > %TEMP%\osg-version.txt
+osgversion --openthreads-soversion-number > %TEMP%\openthreads-so-number.txt
 
 SET /P FLIGHTGEAR_VERSION=<flightgear\version
 SET /P OSG_VERSION=<%TEMP%\osg-version.txt
 SET /P OSG_SO_NUMBER=<%TEMP%\osg-so-number.txt
+SET /P OT_SO_NUMBER=<%TEMP%\openthreads-so-number.txt
 
 ECHO #define FGVersion "%FLIGHTGEAR_VERSION%" > InstallConfig.iss
 ECHO #define OSGVersion "%OSG_VERSION%" >> InstallConfig.iss
 ECHO #define OSGSoNumber "%OSG_SO_NUMBER%" >> InstallConfig.iss
+ECHO #define OTSoNumber "%OT_SO_NUMBER%" >> InstallConfig.iss
 
 set DATA_FILE=FlightGear-%FLIGHTGEAR_VERSION%-data
 
