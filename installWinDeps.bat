0b5855fdeeed4845151a0ef4495853c22670b1e6
Clément de l'Hamaide <clemaez@hotmail.fr>
2013-11-14 02:35:33 +0100

    Superbuild: fix 3rdParty 32/64 compatibility

--- installWinDeps.bat
+++ installWinDeps.bat
@@ -5,8 +5,12 @@ REM Instead we extract to a subdir, and then move the directories we want
 REM using this bat file.
 
 echo %CD%
-
-md 3rdParty
-xcopy /Y /E winDeps/3rdParty 3rdParty
-
+IF EXIST winDeps/3rdParty (
+    md 3rdParty
+    xcopy /Y /E winDeps/3rdParty 3rdParty
+)
+IF EXIST winDeps/3rdParty.x64 (
+    md 3rdParty.x64
+    xcopy /Y /E winDeps/3rdParty.x64 3rdParty.x64
+)
 echo "Done copying Windows deps"
