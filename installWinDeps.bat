ba53b4ce5fe0a236c632ad860c45c3f1e1fbd915
Clement de l'Hamaide <clemaez@hotmail.fr>
2013-11-16 00:07:57 +0100

    Superbuild: use SVN for 3rdParty and Boost + add exit code in installWinDeps.bat if xcopy fails

--- installWinDeps.bat
+++ installWinDeps.bat
@@ -1,5 +1,3 @@
-
-
 REM ExternalProject can't cleanly extract a zip into an existing directory
 REM Instead we extract to a subdir, and then move the directories we want
 REM using this bat file.
@@ -8,9 +6,14 @@ echo %CD%
 IF EXIST winDeps/3rdParty (
     md 3rdParty
     xcopy /Y /E winDeps/3rdParty 3rdParty
+    echo "Done copying Windows deps"
+) ELSE (
+    IF EXIST winDeps/3rdParty.x64 (
+        md 3rdParty.x64
+        xcopy /Y /E winDeps/3rdParty.x64 3rdParty.x64
+        echo "Done copying Windows deps"
+    ) ELSE (
+        echo "Error: Windows deps not found"
+        exit -1
+    )
 )
-IF EXIST winDeps/3rdParty.x64 (
-    md 3rdParty.x64
-    xcopy /Y /E winDeps/3rdParty.x64 3rdParty.x64
-)
-echo "Done copying Windows deps"
