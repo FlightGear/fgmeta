62dd9bc45867d2e1bdc689287745d20b1767079f
James Turner <zakalawe@mac.com>
2014-01-18 14:53:48 +0000

    Switch Windows SG+FG builds to RelWithDebInfo
    This means PDB files will be generated, suitable for post-hoc
    minidump assembly.

--- hudson_win_build64.bat
+++ hudson_win_build64.bat
@@ -13,11 +13,11 @@ md build-fg64
 md build-fgrun64
 cd build-sg64
 cmake ..\SimGear -G "Visual Studio 10 Win64" -DMSVC_3RDPARTY_ROOT=%WORKSPACE% -DCMAKE_INSTALL_PREFIX:PATH=%WORKSPACE%/install/msvc100-64/SimGear -DBOOST_ROOT=%WORKSPACE%/Boost
-cmake --build . --config Release --target INSTALL
+cmake --build . --config RelWithDebInfo --target INSTALL
 
 cd ..\build-fg64
 cmake ..\flightgear -G "Visual Studio 10 Win64" -DMSVC_3RDPARTY_ROOT=%WORKSPACE% -DCMAKE_INSTALL_PREFIX:PATH=%WORKSPACE%/install/msvc100-64/FlightGear -DFLTK_FLUID_EXECUTABLE=%WORKSPACE%/3rdParty/bin/fluid.exe -DBOOST_ROOT=%WORKSPACE%/Boost -DWITH_FGPANEL=OFF -DENABLE_PROFILE=OFF
-cmake --build . --config Release --target INSTALL
+cmake --build . --config RelWithDebInfo --target INSTALL
 
 cd ..\build-fgrun64
 cmake ..\fgrun -G "Visual Studio 10 Win64" -DMSVC_3RDPARTY_ROOT:PATH=%WORKSPACE% -DCMAKE_INSTALL_PREFIX:PATH=%WORKSPACE%/install/msvc100-64/FGRun -DFLTK_FLUID_EXECUTABLE:FILEPATH=%WORKSPACE%/3rdParty/bin/fluid.exe -DGETTEXT_MSGFMT_EXECUTABLE:FILEPATH=%WORKSPACE%/3rdParty/bin/msgfmt.exe -DGETTEXT_MSGMERGE_EXECUTABLE:FILEPATH=%WORKSPACE%/3rdParty/bin/msgmerge.exe -DBOOST_ROOT=%WORKSPACE%/Boost
