d54ef9f452662b4ccc23a0f14a1f9ff8e9521795
Clément de l'Hamaide <clemaez@hotmail.fr>
2014-05-22 22:12:21 +0200

    d&c: and bump version

--- download_and_compile.sh
+++ download_and_compile.sh
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-VERSION="2.25"
+VERSION="2.26"
 
 #######################################################
 # THANKS TO
