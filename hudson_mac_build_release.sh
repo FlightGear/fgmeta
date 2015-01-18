84fdb24d6d8965e9866e2d8aa3eed093fb3a6524
James Turner <zakalawe@mac.com>
2015-01-18 22:56:03 +0000

    Mac launcher is gone.

--- hudson_mac_build_release.sh
+++ hudson_mac_build_release.sh
@@ -47,24 +47,6 @@ popd
 chmod +x $WORKSPACE/dist/bin/osgversion
 
 ################################################################################
-echo "Building Macflightgear launcher"
-
-#OSX_TARGET="10.6"
-# JMT - disabling setting the sysroot since it's breaking things on 
-# current build slave. Sinc ethe slave runs 10.6 natively, we don't 
-# actually need to set these. Real solution would be to use a proper
-# build system for the Mac-launcher of course.
-# -mmacosx-version-min=$OSX_TARGET -isysroot $SDK_PATH
-
-pushd maclauncher/FlightGearOSX
-
-# compile the stub executable
-gcc -o FlightGear main.m \
-    -framework Cocoa -framework RubyCocoa -framework Foundation -framework AppKit
-
-popd
-
-################################################################################
 # run the unlock script now - we need to do this right before code-signing,
 # or the keychain may automatically re-lock after some period of time
 unlock-keychain.sh
