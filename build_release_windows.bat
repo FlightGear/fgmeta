9b72febb482e932e0225f2f1b84567467e060578
Clément de l'Hamaide <clemaez@hotmail.fr>
2015-03-09 23:29:55 +0100

    Windows: add Qt5 SDK path to CMake

--- build_release_windows.bat
+++ build_release_windows.bat
@@ -7,15 +7,30 @@ md build-sg32
 md build-fg32
 md build-fgrun32
 cd build-sg32
-cmake ..\simgear -G "Visual Studio 10" -DMSVC_3RDPARTY_ROOT=%WORKSPACE% -DCMAKE_INSTALL_PREFIX:PATH=%WORKSPACE%/install/msvc100/SimGear -DBOOST_ROOT=%WORKSPACE%/Boost
+cmake ..\simgear -G "Visual Studio 10" ^
+                 -DMSVC_3RDPARTY_ROOT=%WORKSPACE% ^
+                 -DCMAKE_INSTALL_PREFIX:PATH=%WORKSPACE%/install/msvc100/SimGear ^
+                 -DBOOST_ROOT=%WORKSPACE%/Boost
 cmake --build . --config Release --target INSTALL
 
 cd ..\build-fg32
-cmake ..\flightgear -G "Visual Studio 10" -DMSVC_3RDPARTY_ROOT=%WORKSPACE% -DCMAKE_INSTALL_PREFIX:PATH=%WORKSPACE%/install/msvc100/FlightGear -DPNG_LIBRARY=%WORKSPACE%/3rdParty/lib/libpng16.lib -DFLTK_FLUID_EXECUTABLE=%WORKSPACE%/3rdParty/bin/fluid.exe -DBOOST_ROOT=%WORKSPACE%/Boost
+cmake ..\flightgear -G "Visual Studio 10" ^
+                    -DMSVC_3RDPARTY_ROOT=%WORKSPACE% ^
+                    -DCMAKE_INSTALL_PREFIX:PATH=%WORKSPACE%/install/msvc100/FlightGear ^
+                    -DQt5_DIR=%QTSDK32% ^
+                    -DPNG_LIBRARY=%WORKSPACE%/3rdParty/lib/libpng16.lib ^
+                    -DFLTK_FLUID_EXECUTABLE=%WORKSPACE%/3rdParty/bin/fluid.exe ^
+                    -DBOOST_ROOT=%WORKSPACE%/Boost
 cmake --build . --config Release --target INSTALL
 
 cd ..\build-fgrun32
-cmake ..\fgrun -G "Visual Studio 10" -DMSVC_3RDPARTY_ROOT:PATH=%WORKSPACE% -DCMAKE_INSTALL_PREFIX:PATH=%WORKSPACE%/install/msvc100/FGRun -DFLTK_FLUID_EXECUTABLE:FILEPATH=%WORKSPACE%/3rdParty/bin/fluid.exe -DGETTEXT_MSGFMT_EXECUTABLE:FILEPATH=%WORKSPACE%/3rdParty/bin/msgfmt.exe -DGETTEXT_MSGMERGE_EXECUTABLE:FILEPATH=%WORKSPACE%/3rdParty/bin/msgmerge.exe -DBOOST_ROOT=%WORKSPACE%/Boost
+cmake ..\fgrun -G "Visual Studio 10" ^
+               -DMSVC_3RDPARTY_ROOT:PATH=%WORKSPACE% ^
+               -DCMAKE_INSTALL_PREFIX:PATH=%WORKSPACE%/install/msvc100/FGRun ^
+               -DFLTK_FLUID_EXECUTABLE:FILEPATH=%WORKSPACE%/3rdParty/bin/fluid.exe ^
+               -DGETTEXT_MSGFMT_EXECUTABLE:FILEPATH=%WORKSPACE%/3rdParty/bin/msgfmt.exe ^
+               -DGETTEXT_MSGMERGE_EXECUTABLE:FILEPATH=%WORKSPACE%/3rdParty/bin/msgmerge.exe ^
+               -DBOOST_ROOT=%WORKSPACE%/Boost
 cmake --build . --config Release --target INSTALL
 
 cd ..
@@ -25,15 +40,31 @@ md build-sg64
 md build-fg64
 md build-fgrun64
 cd build-sg64
-cmake ..\SimGear -G "Visual Studio 10 Win64" -DMSVC_3RDPARTY_ROOT=%WORKSPACE% -DCMAKE_INSTALL_PREFIX:PATH=%WORKSPACE%/install/msvc100-64/SimGear -DBOOST_ROOT=%WORKSPACE%/Boost
+cmake ..\SimGear -G "Visual Studio 10 Win64" ^
+                 -DMSVC_3RDPARTY_ROOT=%WORKSPACE% ^
+                 -DCMAKE_INSTALL_PREFIX:PATH=%WORKSPACE%/install/msvc100-64/SimGear ^
+                 -DBOOST_ROOT=%WORKSPACE%/Boost
 cmake --build . --config Release --target INSTALL
 
 cd ..\build-fg64
-cmake ..\flightgear -G "Visual Studio 10 Win64" -DMSVC_3RDPARTY_ROOT=%WORKSPACE% -DCMAKE_INSTALL_PREFIX:PATH=%WORKSPACE%/install/msvc100-64/FlightGear -DFLTK_FLUID_EXECUTABLE=%WORKSPACE%/3rdParty/bin/fluid.exe -DBOOST_ROOT=%WORKSPACE%/Boost -DWITH_FGPANEL=OFF -DENABLE_PROFILE=OFF
+cmake ..\flightgear -G "Visual Studio 10 Win64" ^
+                    -DMSVC_3RDPARTY_ROOT=%WORKSPACE% ^
+                    -DCMAKE_INSTALL_PREFIX:PATH=%WORKSPACE%/install/msvc100-64/FlightGear ^
+                    -DQt5_DIR=%QTSDK64% ^
+                    -DFLTK_FLUID_EXECUTABLE=%WORKSPACE%/3rdParty/bin/fluid.exe ^
+                    -DBOOST_ROOT=%WORKSPACE%/Boost ^
+                    -DWITH_FGPANEL=OFF ^
+                    -DENABLE_PROFILE=OFF
 cmake --build . --config Release --target INSTALL
 
 cd ..\build-fgrun64
-cmake ..\fgrun -G "Visual Studio 10 Win64" -DMSVC_3RDPARTY_ROOT:PATH=%WORKSPACE% -DCMAKE_INSTALL_PREFIX:PATH=%WORKSPACE%/install/msvc100-64/FGRun -DFLTK_FLUID_EXECUTABLE:FILEPATH=%WORKSPACE%/3rdParty/bin/fluid.exe -DGETTEXT_MSGFMT_EXECUTABLE:FILEPATH=%WORKSPACE%/3rdParty/bin/msgfmt.exe -DGETTEXT_MSGMERGE_EXECUTABLE:FILEPATH=%WORKSPACE%/3rdParty/bin/msgmerge.exe -DBOOST_ROOT=%WORKSPACE%/Boost
+cmake ..\fgrun -G "Visual Studio 10 Win64" ^
+               -DMSVC_3RDPARTY_ROOT:PATH=%WORKSPACE% ^
+               -DCMAKE_INSTALL_PREFIX:PATH=%WORKSPACE%/install/msvc100-64/FGRun ^
+               -DFLTK_FLUID_EXECUTABLE:FILEPATH=%WORKSPACE%/3rdParty/bin/fluid.exe ^
+               -DGETTEXT_MSGFMT_EXECUTABLE:FILEPATH=%WORKSPACE%/3rdParty/bin/msgfmt.exe ^
+               -DGETTEXT_MSGMERGE_EXECUTABLE:FILEPATH=%WORKSPACE%/3rdParty/bin/msgmerge.exe ^
+               -DBOOST_ROOT=%WORKSPACE%/Boost
 cmake --build . --config Release --target INSTALL
 
 cd ..
