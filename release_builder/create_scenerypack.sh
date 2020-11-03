be5880ee193809b8bb74aeaa02346122361fccf4
James Turner <james@flightgear.org>
2020-11-03 19:57:58 +0000

    Save the modfiied create-scenery-pack script

--- release_builder/create_scenerypack.sh
+++ release_builder/create_scenerypack.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
-#
-# Create the scenery pack for a release distribution
-# Make sure to 
-# * set SCENERY and point it to your local (full) mirror of terrasync scenery
-# * name the SCENERY_PACK correctly
-# * carefully select the tiles to copy, usually 2x2, 2x3 or 3x2 keeps the pack small enough
-#
-SCENERY=/path/to/your/scenery
-SCENERY_PACK=SceneryPack.PHNL
-TILES="w160n[12]0/w???n??"
 
-if [ ! -d "$SCENERY" ]; then
-  echo "Scenery directory not found or not readable"
-  exit 1
-fi
+# the name to create
+SCENERY_PACK=SceneryPack.BIKF
 
-if [ ! -d "$SCENERY"/Objects -o ! -d  "$SCENERY"/Terrain -o ! -d  "$SCENERY"/Airports -o ! -d  "$SCENERY"/Models ]; then
-  echo "Scenery directory does not look like a scenery directory"
-  exit 1
-fi
+# the tiles to select
+TILES="w030n60/w???n??";
+
+# tiles for PHNL for C172 tutorials
+TUTORIAL_TILES="w160n[12]0/w???n??";
 
 rm -f SceneryPack.*.tgz
-ln -s "$SCENERY" ${SCENERY_PACK}
-tar --format=gnu --create --owner=root --group=root --gzip --exclude="**/.dirindex" --file=${SCENERY_PACK}.tgz ${SCENERY_PACK}/Objects/${TILES} ${SCENERY_PACK}/Terrain/${TILES} ${SCENERY_PACK}/Airports ${SCENERY_PACK}/Models
+
+# note the path to the TerraSync root here
+ln -s /var/www/uk-mirror/fgscenery ${SCENERY_PACK}
+
+tar --format=gnu --create --owner=root --group=root --gzip --file=${SCENERY_PACK}.tgz \
+    ${SCENERY_PACK}/Objects/${TILES} \
+    ${SCENERY_PACK}/Terrain/${TILES} \
+    ${SCENERY_PACK}/Objects/${TUTORIAL_TILES} \
+    ${SCENERY_PACK}/Terrain/${TUTORIAL_TILES} \
+    ${SCENERY_PACK}/Airports/B/I/K \
+    ${SCENERY_PACK}/Airports/P/H \
+    ${SCENERY_PACK}/Models
+
 rm ${SCENERY_PACK}
+
+# upload to frs.sourceforge.net /home/frs/project/fquitfl/flightgear/scenery/ 
+
